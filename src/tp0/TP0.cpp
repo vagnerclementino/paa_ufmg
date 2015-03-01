31e0862bda67ce104a0d5b1e4235c152f7366e09
Vagner Clementino <vagner.clementino@gmail.com>
2015-03-01 13:53:33 -0300

    Revert "Merge branch 'master' into tp0: Atualizando a pasta lib devido alteração" Motivo: remoção da pasta tp0 não deveria ocorrer. This reverts commit 65f1c0e8e60224e0d2cd1f0c1944bbebb80e2eb9, reversing changes made to c6e642050dbaa4acd80f750894ce130d3dfbaf5c.

---
+++ src/tp0/TP0.cpp
@@ -0,0 +1,70 @@
+/*
+ * TP1.cpp
+ *
+ *  Created on: 02/02/2015
+ *      Author: vagner
+ */
+
+#include "TP0.h"
+#include "../lib/TrabalhoPratico.h"
+#include <iostream>
+#include "../lib/PAAException.h"
+#include <new>
+#include <sstream>
+#include <unistd.h>
+
+namespace PAA {
+
+TP0::TP0():TrabalhoPratico()
+{
+ //Do nothing
+
+
+}
+
+TP0::~TP0() {
+	// TODO Auto-generated destructor stub
+}
+
+void TP0::run(void ){
+	try {
+			this->showUserMessage("Iniciando a execução do TP1");
+
+			this->showUserMessage("Ola Mundo");
+
+			sleep(10);
+
+			this->showUserMessage("Finalizando a execução do TP1");
+
+			this->setFinalTime();
+
+	} catch (const std::exception& e) {
+
+		std::stringstream ss;
+		ss << "Erro durante a alocaco de memoria. Tipo de excecao: " << e.what();
+		throw PAA::PAAException(ss.str());
+
+
+	}
+
+
+}
+
+void TP0::showUserMessage(const std::string& message){
+
+	PAA::TrabalhoPratico::showUserMessage(message);
+
+}
+
+void TP0::showStatistics(void){
+
+	PAA::TrabalhoPratico::showStatistics();
+}
+
+void TP0::setFinalTime(void){
+
+	PAA::TrabalhoPratico::setFinalTime();
+
+}
+
+}/* namespace PAA */
