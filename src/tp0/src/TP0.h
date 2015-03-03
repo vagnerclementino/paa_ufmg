facab16dcbc7b266955179424df1861419fe5588
Vagner Clementino <vagner.clementino@gmail.com>
2015-03-03 20:17:36 -0300

    Alteração na estrutura do projeto, criado a src dentro de tp0

---
+++ src/tp0/src/TP0.h
@@ -0,0 +1,28 @@
+/*
+ * TP1.h
+ *
+ *  Created on: 02/02/2015
+ *      Author: vagner
+ */
+
+#ifndef TP1_H_
+#define TP1_H_
+
+#include "../../lib/TrabalhoPratico.h"
+
+namespace PAA {
+
+class TP0: public TrabalhoPratico {
+public:
+	TP0();
+	virtual ~TP0();
+	void run(void);
+	void showUserMessage(const std::string&);
+	void showStatistics(void);
+protected:
+	void setFinalTime(void);
+};
+
+} /* namespace PAA */
+
+#endif /* TP1_H_ */
