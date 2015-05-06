353eb380a2d3e7517151e9e495987e30723dff12
Vagner Clementino <vagner.clementino@gmail.com>
2015-05-06 10:03:19 -0300

    Alterado a string de localização do arquivo

--- src/tp_paradigmas/src/TPParadigmas.cpp
+++ src/tp_paradigmas/src/TPParadigmas.cpp
@@ -31,8 +31,8 @@ void TPParadigmas::run(void ){
 	try {
 
 			PAA::TPInstance instances;
-			std::string fileName = "/home/vagner/workspace/paa_ufmg/src/tp_paradigmas/inputs/inputs.txt";
-			this->showUserMessage("Iniciando a execução.");
+			std::string fileName = "/home/vagner/Vagner/workspace/paa_ufmg/src/tp_paradigmas/inputs/inputs.txt";
+		     this->showUserMessage("Iniciando a execução.");
 
 			instances.load(fileName);
 			ss << "Tamanho da instância " << instances.getSize();
